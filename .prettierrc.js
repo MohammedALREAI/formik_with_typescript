6ffd93740ac888fb63e5e2d5f7ead84084662fef
Mohammed alreai <mhamad.aa1997.aa@gmail.com>
2021-12-07 13:51:40 +0200

    just  validation  without  delate  and  update

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports =  {
+    trailingComma: 'all',
+    doubleQuote: false,
+    printWidth: 120,
+    tabWidth: 4,
+    quoteProps: 'preserve',
+  useTabs: false,
+ };
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { IndexForm } from './Component'
+
+export default class App extends React.Component {
+    render() {
+        return <IndexForm />
+    }
+}
